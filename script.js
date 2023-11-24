c044a7e1c44d39b7b01f8adfba4705d0826fb1f9
Davy Pereira <davypereira1802@gmail.com>
2023-11-24 00:57:02 -0300

    primeira versão

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adiciona a classe 'show' após o carregamento da página para iniciar a animação
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.typewriter').classList.add('show');
+});
